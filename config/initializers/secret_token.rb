131cc6a9168c030a396a9f9ed007eb7126928af0
namakesugi <info@namakesugi.net>
2012-09-02 21:17:05 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctopusSample::Application.config.secret_token = 'b5f89f77ebc90b271012ca7b13a2de089f0b1663d91594b606a89eabeb94a92309a961e87074bced4d9cba8e779c01f5cc180e0e80e2e13e5a6c9da380914ce1'
